686f39e2af25a07304fa4a497b5508eface6def9
Ivan Gromov <summer.is.gone@gmail.com>
2011-06-11 02:33:25 +0600

    Added flatpages recipe

--- rscms/core/management/commands/enroll.py
+++ rscms/core/management/commands/enroll.py
@@ -5,6 +5,7 @@ from optparse import make_option
 from rscms.core.models import Project
 from rscms.recipes.base_project.models import BaseProjectRecipe
 from rscms.recipes.admin.models import DjangoAdminRecipe
+from rscms.recipes.flatpages.models import FlatpagesRecipe
 
 class Command(BaseCommand):
     help = "Test site build"
@@ -27,9 +28,11 @@ class Command(BaseCommand):
 
         base_project_recipe = BaseProjectRecipe(project, project.name)
         admin_recipe = DjangoAdminRecipe(project)
+        flatpages_recipe = FlatpagesRecipe(project)
 
         project.recipes.append(base_project_recipe)
         project.recipes.append(admin_recipe)
+        project.recipes.append(flatpages_recipe)
 
         project.copy_raw()
         project.render()
---
+++ rscms/recipes/flatpages/models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from core.helpers import AppRecipe, url_helper
+
+
+class FlatpagesRecipe(AppRecipe):
+    _file = __file__
+    appname = 'django.contrib.flatpages'
+    installed_apps = [
+        'django.contrib.flatpages',
+    ]
+    middleware_classes = 'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
